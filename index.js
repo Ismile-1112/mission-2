b671283ea7c4f1b297f90935126b916db7ebf0c4
Vismay Parikh <vismayparikh1112@gmail.com>
2021-06-29 11:07:51 +0530

    debugging is continue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const newCard = ({id, imageUrl, taskTitle, taskType, taskDescription,}) => `<div
       <span class="badge bg-primary">${taskType}</span>
     </div>
     <div class="card-footer text-muted">
-        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+        <button type="button" id=${id} class="btn btn-outline-primary float-end">Open Task</button>
     </div>
 </div>
 </div>`;
@@ -107,5 +107,50 @@ const editCard = (event) => {
     taskTitle.setAttribute("contenteditable", "true");
     taskDescription.setAttribute("contenteditable", "true");
     taskType.setAttribute("contenteditable", "true");
-    submitButton.innerHTML = "Save Changes"
+    submitButton.setAttribute("onclick", "saveEditChanges.apply(this, arguments)");
+    submitButton.innerHTML = "Save Changes";
 };
+
+const saveEditChanges = (event) => {
+    event = window.event;
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    let parentElement;
+
+    if(tagname === "Button") {
+        parentElement = event.target.parentNode.parentNode;
+    } else { 
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    const updatedData = {
+        taskTitle: taskTitle.innerHTML,
+        taskType: taskType.innerHTML, 
+        taskDescription: taskDescription.innerHTML,
+    };
+
+    globalStore = globalStore.map((task) => {
+        if (task.id === targetID) {
+            return{
+                id: task.id,
+                imageUrl: task.imageUrl,
+                taskTitle: updatedData.taskTitle,
+                taskType: updatedData.taskType,
+                taskDescription: updatedData.taskDescription,
+            };
+        }
+        return task; // Important
+    });
+    updateLocalStorage();
+    taskTitle.setAttribute("contenteditable", "false");
+    taskDescription.setAttribute("contenteditable", "false");
+    taskType.setAttribute("contenteditable", "false");
+    submitButton.removeAttribute("onclick");
+    submitButton.innerHTML = "Open Task";
+}; 
